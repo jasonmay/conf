fd8c0929e2130bd0ef3149712c5db8b2e32535cd
Jason May <jason.a.may@gmail.com>
2010-05-21 09:31:27 -0400

    better module skeleton

--- vim/vim/skeletons/module.pm
+++ vim/vim/skeletons/module.pm
@@ -3,7 +3,31 @@ package Foo;
 use Moose;
 use namespace::autoclean;
 
+=head1 NAME
+
+Foo -
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
 
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+__END__
+
+=head1 METHODS
+
+
+=head1 AUTHOR
+
+Jason May C<< <jmay@silverbacknetwork.com> >>
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
