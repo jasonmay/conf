7fcf73ab51cfb01b7c3b6f3ec3cdf55f169b2bfe
Jason May <jason.a.may@gmail.com>
2010-09-29 14:15:22 -0400

    indentation

--- zsh/scripts/201-aliases-kinklink.zsh
+++ zsh/scripts/201-aliases-kinklink.zsh
@@ -1,45 +1,50 @@
 case $MY_HOSTNAME in
     kinklink.local)
-alias ls='gls --color=auto'
-alias sort='gsort'
-alias uniq='guniq'
-alias cp='gcp'
-alias cat='gcat'
-alias basename='gbasename'
-alias chown='gchown'
-alias chmod='gchmod'
-alias du='gdu'
+
+    alias ls='gls --color=auto'
+    alias sort='gsort'
+    alias uniq='guniq'
+    alias cp='gcp'
+    alias cat='gcat'
+    alias basename='gbasename'
+    alias chown='gchown'
+    alias chmod='gchmod'
+    alias du='gdu'
+    alias v='sudo port variants'
+    alias ar='sudo /opt/local/apache2/bin/apachectl restart && echo uhh success'
+    alias i='sudo port -v install'
 
 #alias mysqlstart='/Users/jasonmay/.brew/Cellar/mysql/5.1.49/share/mysql/mysql.server start'
 #alias mysqlstop='/Users/jasonmay/.brew/Cellar/mysql/5.1.49/share/mysql/mysql.server stop'
 
-alias i='sudo port -v install'
-function s() {
-    [ -z "$*" ] && return
+    function cpan-outdated() {
+        /Users/jasonmay/.perl5/perlbrew/bin/cpan-outdated | grep -v 'Catalyst-Controller-DBIC-API'
+    }
 
-    echo "searching macports..."
-    sudo port search $*
-    echo "searching homebrew..."
-    brew search $*
-}
-alias v='sudo port variants'
-alias ar='sudo /opt/local/apache2/bin/apachectl restart && echo uhh success'
+    function s() {
+        [ -z "$*" ] && return
 
-function nasadbstagingtunnel() {
-    ssh -fN -L $NASA_DBLOCALPORT:127.0.0.1:$NASA_DBREMOTEPORT $DEVSERVER
-}
-function mogiletunnel() {
-    ssh -fN -L $NASA_MOGPORT:127.0.0.1:$NASA_MOGPORT $DEVSERVER
-}
-function mogstoretunnel() {
-    ssh -fN -L $NASA_MOGSTOREPORT:127.0.0.1:$NASA_MOGSTOREPORT $DEVSERVER
-}
+        echo "searching macports..."
+        sudo port search $*
+        echo "searching homebrew..."
+        brew search $*
+    }
 
-function setup_tunnels() {
-    nasadbstagingtunnel
-    mogiletunnel
-    mogstoretunnel
-}
+    function nasadbstagingtunnel() {
+        ssh -fN -L $NASA_DBLOCALPORT:127.0.0.1:$NASA_DBREMOTEPORT $DEVSERVER
+    }
+    function mogiletunnel() {
+        ssh -fN -L $NASA_MOGPORT:127.0.0.1:$NASA_MOGPORT $DEVSERVER
+    }
+    function mogstoretunnel() {
+        ssh -fN -L $NASA_MOGSTOREPORT:127.0.0.1:$NASA_MOGSTOREPORT $DEVSERVER
+    }
+
+    function setup_tunnels() {
+        nasadbstagingtunnel
+        mogiletunnel
+        mogstoretunnel
+    }
 ;;
 
 esac
