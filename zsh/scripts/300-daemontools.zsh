9ad4bb767bd48265565ba89c88cc34fed08d44c1
Jason May <jason.a.may@gmail.com>
2010-10-19 10:21:02 -0400

    precedence matters here

--- zsh/scripts/300-daemontools.zsh
+++ zsh/scripts/300-daemontools.zsh
@@ -1,4 +1,6 @@
 check_exec dtach \
     && check_exec svscan \
-    && running svscan \
-    || dtach -nc $HOME/.svscansocket svscan $HOME/.services
+    && (
+        running svscan \
+        || dtach -nc $HOME/.svscansocket svscan $HOME/.services
+    )
