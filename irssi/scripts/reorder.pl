f17f4d40fd24934f3fa8fe96d29e749baffc041f
Jason May <jason.a.may@gmail.com>
2011-04-29 20:13:46 -0400

    bit of styling

--- irssi/scripts/reorder.pl
+++ irssi/scripts/reorder.pl
@@ -31,74 +31,56 @@ use POSIX 'strftime';
     license     => "GPL",
 );
 
-sub doFilenameFixing
-{
+sub doFilenameFixing {
     my ($filename) = @_;
 
     my $i=1;
 
-    unless (length($filename))
-    {
-        if (1)
-        {
-            my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-            $filename = POSIX::strftime("%y%m%d", $sec, $min, $hour, $mday, $mon, $year);
-        } else {
-            $filename = "default";
-        }
+    unless (length($filename)) {
+        my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst)
+            = localtime(time);
+
+        $filename = POSIX::strftime("%y%m%d",
+                        $sec, $min, $hour, $mday, $mon, $year);
     }
 
     my $glob = glob($filename);
     $filename = $glob if $glob;
 
-    if ($filename =~ /\//)
-    {
-        unless ($filename =~ /^\//)
-        {
-            print "I don't like /'s in filenames. Unless you want to specify an absolute path.";
-            return;
-        }
-        # Let it go?
+    if ($filename !~ m[^/] and $filename =~ m[/]) {
+        # XXX why is this bad exactly...?
+        die "I don't like /'s in filenames. " .
+            "Unless you want to specify an absolute path."
     }
 
-    $filename .= '.layout' unless ($filename =~ /.layout$/);
-
+    $filename .= '.layout' unless $filename =~ /.layout$/;
     my $path = Irssi::settings_get_str('layout_savepath');
-    $path .= '/' unless ($path =~ /\/$/);
-    $filename = $path . $filename unless ($filename =~ /\//);
+    $path .= '/' unless $path =~ m[/$];
+    $filename = $path . $filename unless $filename =~ m[/];
 
     return $filename;
 }
 
-sub canReadFile
-{
+sub canReadFile {
     my ($filename) = @_;
-    unless (-f $filename)
-    {
-        print "No such file $filename";
-        return;
-    }
-    unless (-r $filename)
-    {
-        print "Can not read file $filename";
-        return;
-    }
+
+    warn "No such file $filename"      unless -f $filename;
+    warn "Can not read file $filename" unless -r $filename;
+
     return 1;
 }
 
 # Save a list of refnum and window info to file
-sub cmd_layout_save
-{
+sub cmd_layout_save {
     my ($filename, $data, $more) = @_;
-    my $FH;
+    my $fh;
 
     $filename = doFilenameFixing($filename);
-    return unless ($filename);
+    return unless $filename;
 
     # Order by ref. Print ref and an id tag
     my %layout;
-    for my $win (Irssi::windows())
-    {
+    for my $win (Irssi::windows()) {
         my $id = $win->{name} ||
         "$win->{active}->{name}:$win->{active}->{server}->{tag}";
 
@@ -106,59 +88,51 @@ sub cmd_layout_save
     }
     my @refs = sort { $a <=> $b } keys %layout;
 
-    unless(open $FH, ">", $filename)
-    {
-        print "Can not open $filename";
-        return;
-    }
+    open my $fh, ">", $filename
+        or die "Can not open $filename";
 
     foreach my $line (1 .. $refs[-1]) {
-        printf $FH "%s\n", exists($layout{$line}) ? $layout{$line} : '';
+        printf $fh "%s\n", exists($layout{$line}) ? $layout{$line} : '';
     }
 
-    close $FH;
+    close $fh;
     print "Layout saved to $filename";
 }
 
 # Load a list and use it to reorder
-sub cmd_layout_load
-{
+sub cmd_layout_load {
     # Check filename supplied, exists and readable
     my ($filename, $data, $more) = @_;
     $filename = doFilenameFixing($filename);
-    return unless ($filename);
 
+    return unless $filename;
     return unless canReadFile($filename);
 
     my @layout;
-    my ($id, $FH);
 
     # Pull the refnum and id
-    unless(open $FH, "<", $filename)
-    {
-        print "Can not open file $filename.";
-        return;
-    }
-    while (my $line = <$FH>)
-    {
+    open my $fh, "<", $filename
+        or die "Can not open file $filename.";
+
+    while (my $line = <$fh>) {
         chomp $line;
         next unless $line =~ /\S/;
-        my $id = $line;
-        next unless $id;
 
-        push @layout, {refnum => $., id => $id};
+        # track current line in $fh, and $line itself
+        push @layout, {refnum => $., id => $line};
     }
-    close $FH;
+    close $fh;
 
     # For each layout item from the file, find the window and set it's ref to that number
-    for my $position (sort {$a->{'refnum'} <=> $b->{'refnum'}} @layout)
-    {
-        for my $win (Irssi::windows())
-        {
-            $id = $win->{'name'} ? $win->{'name'} : $win->{'active'}->{'name'} . ":" . $win->{'active'}->{'server'}->{'tag'};
-            if (($id eq $position->{'id'}) or ($id . "2" eq $position->{'id'}))
-            {
-                $win->set_refnum($position->{'refnum'});
+    for my $position (sort {$a->{'refnum'} <=> $b->{'refnum'}} @layout) {
+        for my $win (Irssi::windows()) {
+            my $chan = $win->{active}->{name};
+            my $network = $win->{active}->{server}->{tag};
+
+            my $id = $win->{name} || "$chan:$network";
+
+            if ($id eq $position->{id} or $id.'2' eq $position->{id}) {
+                $win->set_refnum($position->{refnum});
                 last;
             }
         }
@@ -171,6 +145,5 @@ Irssi::settings_add_bool('misc', 'layout_loadonstart', 1);
 Irssi::command_bind( 'layout_save', 'cmd_layout_save' );
 Irssi::command_bind( 'layout_load', 'cmd_layout_load' );
 
-cmd_layout_load() if (Irssi::settings_get_bool('layout_loadonstart'));
-
-
+# XXX not really sure what this is aiming to do, but I don't want it
+#cmd_layout_load() if (Irssi::settings_get_bool('layout_loadonstart'));
