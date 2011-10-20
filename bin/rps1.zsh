b8b32e8962395b130d594baaeb19963d480f4ef2
Jason May <jason.a.may@gmail.com>
2011-10-20 16:20:46 -0400

    In the case of a detached head, show the SHA in the prompt

--- bin/rps1.zsh
+++ bin/rps1.zsh
@@ -4,7 +4,9 @@ STATUS="$(git status --porcelain 2>/dev/null)"
 [[ $? != 0 ]] && exit;
 
 BRANCH="$(git symbolic-ref HEAD 2>/dev/null)"
-BRANCH="<${BRANCH#refs/heads/}>"
+BRANCH="${BRANCH#refs/heads/}"
+if [ -z "$BRANCH" ]; then BRANCH="%{\e[32m%}$(git rev-parse --short HEAD)%{\e[31m%}"; fi
+BRANCH="<$BRANCH>"
 BRANCH="${BRANCH/<master>/#}"
 if [ "x$STATUS" = "x" ]
 then
