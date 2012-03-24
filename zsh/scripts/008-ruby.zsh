9b988bf647220d5fb43257c4ac51158acba2a5bb
Jason May <jason.a.may@gmail.com>
2012-03-24 16:13:08 -0400

    After a fresh install I am using 1.9.3 as my default now

--- zsh/scripts/008-ruby.zsh
+++ zsh/scripts/008-ruby.zsh
@@ -1,3 +1,3 @@
-[ -e $HOME/.rvm/scripts/rvm ] && source $HOME/.rvm/scripts/rvm && rvm use -q ruby-1.9.2 > /dev/null
+[ -e $HOME/.rvm/scripts/rvm ] && source $HOME/.rvm/scripts/rvm && rvm use -q ruby-1.9.3 > /dev/null
 
 check_exec 'hub' && eval $(hub alias -s `basename $SHELL`)
