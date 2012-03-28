84ecb53c9b54745be6f612b48b16e030c21f1d4f
Jason May <jason.a.may@gmail.com>
2012-03-28 10:03:50 -0400

    'hub alias -s' just works

--- zsh/scripts/008-ruby.zsh
+++ zsh/scripts/008-ruby.zsh
@@ -1,3 +1,3 @@
 [ -e $HOME/.rvm/scripts/rvm ] && source $HOME/.rvm/scripts/rvm && rvm use -q ruby-1.9.3 > /dev/null
 
-check_exec 'hub' && eval $(hub alias -s `basename $SHELL`)
+check_exec 'hub' && eval "$(hub alias -s)"
