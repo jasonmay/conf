cade4a72640f6c531d96de61d5b55cea4467e8db
Jason May <jason.a.may@gmail.com>
2012-05-04 13:08:03 -0400

    Make my build vim script a little less magical

--- bin/rebuild-vim.sh
+++ bin/rebuild-vim.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 make distclean || exit 1
-hg pull || exit 1
-hg update -C || exit 1
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 rvm use system
 
-./configure --without-x --disable-gui --disable-xim --disable-darwin --enable-perlinterp --enable-rubyinterp --enable-multibyte
+./configure --without-x --disable-gui --disable-xim --disable-darwin  --enable-rubyinterp --enable-multibyte
 make
 
 sudo make install
