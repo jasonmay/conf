10cebbee17953bf258295c54d982580b21710f13
Jason May <jason.a.may@gmail.com>
2012-10-24 14:47:20 -0400

    stop using RPS1
    OMZ gives me all this information in PS1 now.

--- zsh/scripts/006-prompt.zsh
+++ zsh/scripts/006-prompt.zsh
@@ -1,5 +1,5 @@
-PROMPT='%n@%m %~ $ ';
-check_exec 'rps1.zsh' && function precmd() {
-    RPROMPT=`rps1.zsh`
-}
+#PROMPT='%n@%m %~ $ ';
+#check_exec 'rps1.zsh' && function precmd() {
+#    RPROMPT=`rps1.zsh`
+#}
 
