3306c8f5695d69ba1eeca8adb60b9d7665f87cc3
Jason May <jason.may@teamaol.com>
2013-11-08 12:58:36 -0500

    Compile a python interpreter into vim

--- bin/rebuild-vim.sh
+++ bin/rebuild-vim.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export ARCHFLAGS="-arch i386"
-make distclean || exit 1
+#make distclean || exit 0
 
 #[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 #rvm use system
 
-./configure --without-x --disable-gui --disable-xim --disable-darwin  --enable-rubyinterp --enable-multibyte
+./configure --without-x --disable-gui --disable-xim --disable-darwin  --enable-rubyinterp --enable-pythoninterp --enable-multibyte
 make
 
 sudo make install
