728ce31bb18677731012b3dfa0554776937c155e
Jason May <jason.a.may@gmail.com>
2014-11-25 18:44:56 +0000

    Remove daemontools config

--- zsh/scripts/300-daemontools.zsh
+++
@@ -1,9 +0,0 @@
-
-[ -z "$(pidof svscan)" ] && rm -f $HOME/.svscansocket
-
-check_exec dtach               \
-    && check_exec svscan       \
-    && [ -z "$(pidof svscan)" ] \
-    && (
-        dtach -n $HOME/.svscansocket svscan $HOME/.services
-    )
